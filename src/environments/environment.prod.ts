6f3ecdcd2707bc382b53eea4102a38cd83f551e7
Алексей Белов <Alenorze@gmail.com>
2017-11-19 14:28:18 +0200

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true
+  firebase: {
+    apiKey: 'AIzaSyDGNYDx6PEnhBh4hXUQ0zKeO8zIWL2Ymoc',
+    authDomain: 'fir-bs-5ec3b.firebaseapp.com',
+    databaseURL: 'https://fir-bs-5ec3b.firebaseio.com',
+    projectId: 'fir-bs-5ec3b',
+    storageBucket: 'fir-bs-5ec3b.appspot.com',
+    messagingSenderId: '857523633704'
+  }
+};
